795ec37f42cf9c0c7ac97e3265d443f7bcd6e597
Manohar-Maharshi <netwitch1012@gmail.com>
2025-01-11 20:51:52 +0530

    feat: integrate svelte-meta-tags for improved SEO and dynamic metadata management

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-	export const prerender = true;
-
 	import '../app.css';
-	let { children } = $props();
+
+	import { page } from '$app/state';
+	import { deepMerge, MetaTags } from 'svelte-meta-tags';
+
+	let { data, children } = $props();
+
+	let metaTags = $derived(deepMerge(data.baseMetaTags, page.data.pageMetaTags));
 </script>
+
+<MetaTags {...metaTags} />
 
 {@render children()}
---
+++ src/routes/+layout.ts
@@ -0,0 +1,35 @@
+import type { MetaTagsProps } from 'svelte-meta-tags';
+import type { LayoutLoad } from './$types';
+
+export const load = (async ({ url }) => {
+	const baseMetaTags = Object.freeze({
+		title: 'Word Sort',
+		titleTemplate: '%s - Alphabetical Text Sorter',
+		url: new URL(url.pathname, url.origin).href,
+		canonical: new URL(url.pathname, url.origin).href,
+		description:
+			'Sort the letters of each word in your text with Word Sort! Paste your text, and let us do the magic.',
+		author: 'Manohar Maharshi',
+		robots: 'index, follow',
+		twitter: {
+			card_type: 'summary',
+			site: '@mmmmaharshi',
+			creator: '@mmmmaharshi',
+			title: 'Word Sort - Alphabetical Text Sorter',
+			description:
+				'Sort the letters of each word in your text with Word Sort! Paste your text, and let us do the magic.'
+		},
+		openGraph: {
+			title: 'Word Sort - Alphabetical Text Sorter',
+			description:
+				'Sort the letters of each word in your text with Word Sort! Paste your text, and let us do the magic.',
+			url: 'https://wordsort.vercel.app/',
+			type: 'website',
+			siteName: 'Word Sort'
+		}
+	}) satisfies MetaTagsProps;
+
+	return {
+		baseMetaTags
+	};
+}) satisfies LayoutLoad;
